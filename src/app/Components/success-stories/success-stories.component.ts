2b296f72503b95efba0fc8eeb5e34692190e021a
tokaehab798 <tokaehab798@gmail.com>
2025-07-21 15:52:56 +0300

    some general updates for responsive layout

--- src/app/Components/success-stories/success-stories.component.ts
+++ src/app/Components/success-stories/success-stories.component.ts
@@ -14,20 +14,19 @@ export class SuccessStoriesComponent {
 SuccessStories = [
   {
     paragraph:
-      "Tanmeyah has become a catalyst for change in underserved communities, empowering people with the tools they need to succeed. Through our financing solutions, individuals have launched small enterprises, created jobs, and supported their families. These stories reflect our mission to drive sustainable impact, one life at a time.",
-    personLeft: [
-      {
-        name: "Yasser ",
+      "At Tanmeyah, we’re not just looking to help small business grow; we’re in the business of giving back to communities, fueling individual success, and changing lives. Our financing programs have allowed men and women to turn dreams into businesses realities, and we are proud to have helped thousands achieve social mobility by giving them the means to start a new life. Meet some of the inspiring men and women who have turned their lives into success stories.15 years of helping a unique class of entrepreneurs and their businesses",
+ 
+    Images: [
+        {
+        name: "yasser",
         image: "assets/images/story1.svg"
-      }
-    ],
-    peopleRight: [
+      },
       {
-        name: "Mohamed & Hossam",
+        name: "Mohamed & Hassan",
         image: "assets/images/story_2.svg"
       },
       {
-        name: "Hend & Sameha",
+        name: "samiha & Hend ",
         image: "assets/images/story_3.svg"
       }
     ]
@@ -35,40 +34,38 @@ SuccessStories = [
   {
     paragraph:
       "Every loan we provide is an opportunity for someone to build a brighter future. From bakery owners to tailors and tech repair entrepreneurs, our clients prove that ambition, when paired with support, can lead to incredible achievements. These stories are a tribute to their resilience and growth.",
-    personLeft: [
-      {
-        name: "Khaled & Rania",
-        image: "assets/images/story7.svg"
-      }
-    ],
-    peopleRight: [
-      {
-        name: "Mostafa",
-        image: "assets/images/story8.svg"
+  
+    Images: [
+        {
+        name: "Omar & safa",
+        image: "assets/images/story1.svg"
       },
       {
-        name: "Layla & Yasmin",
-        image: "assets/images/story9.svg"
+        name: "Hana",
+        image: "assets/images/story_2.svg"
+      },
+      {
+        name: "Mahmoud & Nabil",
+        image: "assets/images/story_3.svg"
       }
     ]
   },
   {
     paragraph:
       "Over the past 15 years, Tanmeyah has supported thousands in unlocking their full potential. Our clients are not just business owners—they are role models in their communities. These stories celebrate their journey from struggle to success and the hope they now inspire in others.",
-    personLeft: [
-      {
-        name: "Omar & Salma",
-        image: "assets/images/story10.svg"
-      }
-    ],
-    peopleRight: [
-      {
-        name: "Hana",
-        image: "assets/images/story11.svg"
+ 
+    Images: [
+        {
+        name: "Omar & Samira",
+        image: "assets/images/story1.svg"
       },
       {
-        name: "Mahmoud & Nabil",
-        image: "assets/images/story12.svg"
+        name: "Hana & ahmed",
+        image: "assets/images/story_2.svg"
+      },
+      {
+        name: "Mahmoud & samir",
+        image: "assets/images/story_3.svg"
       }
     ]
   }
@@ -81,15 +78,21 @@ SuccessStories = [
   }
   currentIndex = 0;
 
+
 nextStory() {
-    let first = this.SuccessStories.shift()
-    if (first) this.SuccessStories.push(first)
+  if (this.currentIndex < this.SuccessStories.length - 1) {
+    this.currentIndex++;
+  } else {
+    this.currentIndex = 0;
+  }
 }
 
 prevStory() {
-  let last = this.SuccessStories.pop();
-    if (last) this.SuccessStories.unshift(last);
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+  } else {
+    this.currentIndex = this.SuccessStories.length - 1; 
+  }
 }
-
 
 }
